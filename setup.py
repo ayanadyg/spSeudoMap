d74a18073da9fa0418be2add3ebe76bd18990b91
ayanadyg <145293409+ayanadyg@users.noreply.github.com>
2023-09-17 23:22:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author = "Sungwoo Bae, Hongyoon Choi",
     install_requires = ["scanpy==1.5.1","pandas==1.3.5","numpy==1.22.0",
                         "h5py==2.10.0", "jupyter",
-                        "keras==2.3.1", "tensorflow==2.12.0", "tensorflow-gpu==1.14.0"]
+                        "keras==2.3.1", "tensorflow==2.12.0", "tensorflow-gpu==2.12.0"]
 )
